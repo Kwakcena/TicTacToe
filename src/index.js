eb6e1f297fd56cae6dd836c4e28adddeec5b38f7
Kwakcena <rhkrgudwh@gmail.com>
2020-02-06 00:11:25 +0900

    Alert function called when we click square component
    게임판의 사각형 요소를 클릭 시 x로 바꾸기 전에 onClick 이벤트를 설정하여
    alert 함수가 호출되는 테스트를 작성한다.
    
    이는 사용자와 상호작용하는 컴포넌트를 만드는 첫 번째 단계이다.

--- src/index.js
+++ src/index.js
@@ -51,7 +51,16 @@ class Board extends React.Component {
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          alert("click");
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
