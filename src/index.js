1d0376b22cbeb756802f869207f2d06fcab51798
Kwakcena <rhkrgudwh@gmail.com>
2020-02-06 15:28:21 +0900

    Add handleClick method and square component use onClick event
    - Square component에서는 Board에서 전달받은 onClick 이벤트를 사용함.
    - 그 이벤트는 Board의 handleClick 으로 정의함.
    - Square component에서는 state를 관리하지 않으므로 생성자는 삭제됨.
    
    Square 를 클릭할 때 발생하는 변화가 필요하다. Board 컴포넌트는 어떤
    사각형이 채워졌는지를 여부를 관리하므로 Square가 Board를 변경할 방법이
    필요하다. 컴포넌트는 자신이 정의한 state에만 접근할 수 있으므로
    Square에서 Board의 state를 직접 변경할 수 없다.
    그래서 Board에서 Square로 함수를 전달하고 Square는 사각형을 클릭할 때
    함수를 호출하도록 했다.
    
    Square 컴포넌트가 더 이상 state를 유지하지 않기 때문에 Square 컴포넌트는
    Board 컴포넌트에서 값을 받아 클릭될 때 Board 컴포넌트로 정보를 전달한다.
    React 용어로 Square 컴포넌트는 이제 제어되는 컴포넌트이다.

--- src/index.js
+++ src/index.js
@@ -26,8 +26,19 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -57,22 +68,10 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          this.setState({ value: "X" });
-        }}
-      >
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
