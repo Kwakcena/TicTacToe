caea6bc2cca49a93ec13d84798a87a73b555b2ce
Kwakcena <rhkrgudwh@gmail.com>
2020-02-06 16:22:39 +0900

    Storing a History of Moves and Lifting State Up, Again
    게임에서 이전 차례로 시간을 되돌릴 수 있는 작업을 추가한다.
    
    동작에 대한 기록을 저장하기 위해 squares 배열을 직접 변경하지 않고
    매 동작마다 복사본을 만들었다. 이런 기록들을 history라는 다른 배열에
    모두 담아 둘 것이다.
    
    따라서 이전 동작에 대한 리스트를 보여주려면 최상위 단계의 Game
    컴포넌트가 필요하다.
    - Game 컴포넌트에 생성자로 history 배열을 둔다.
    - 자식 Board 컴포넌트에서 squares state를 더 이상 사용하지 않는다.
    - 이를 통해 Game 컴포넌트는 Board의 데이터를 완벽히 제어한다.
    - Board는 이제 사용하는 입장이므로 state를 props로 바꾼다.
    - Game 컴포넌트의 render 함수를 가장 최근 기록을 사용하도록 업데이트
      한다.
    - Game 컴포넌트가 게임의 상태를 렌더링하기 때문에 Board의 render
      함수에서 중복되는 코드를 제거했다.
    - Game에서 Board에 handleClick 이벤트를 전달하므로 handleClick 함수를
      Game 컴포넌트로 옮긴다.

--- src/index.js
+++ src/index.js
@@ -3,55 +3,41 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    // slice() 메서드는 어떤 배열의 begin 부터 end까지(end 미 포함)에 대한 얕은 복사본을 새로운
-    // 배열 객체로 반환해준다. 원본 배열은 수정되지 않는다.
-    const squares = this.state.squares.slice();
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      squares: squares,
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
       xIsNext: !this.state.xIsNext
     });
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -60,8 +46,32 @@ class Board extends React.Component {
     }
 
     return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
